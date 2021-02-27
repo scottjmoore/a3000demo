e33f632e351a5080df88e16cb0ec0a36bc592e48
Scott Moore (Arch) <scottmoore@archlinux-epsilon.broadband>
2021-02-27 00:52:11 +0000

    Change to std syntax module to allow use of hex literals

--- demo.asm
+++ demo.asm
@@ -1,91 +1,61 @@
 ; SWI function values
-OS_WriteC           = $00
-OS_WriteS           = $01
-OS_NewLine          = $03
-OS_Byte             = $06
-OS_Exit             = $11
+.set OS_WriteC,0x00
+.set OS_WriteS,0x01
+.set OS_NewLine,0x03
+.set OS_Byte,0x06
+.set OS_Exit,0x11
 
 ; VDU function values
-VDU_TextColour      = 17
-VDU_Palette         = 19
-VDU_DefaultColours  = 20
-VDU_Mode            = 22
+.set VDU_TextColour,17
+.set VDU_Palette,19
+.set VDU_DefaultColours,20
+.set VDU_Mode,22
 
 ; VDU macro, can accept upto 6 parameters
-VDU:    macro
-        nolist
-        if NARG=1           ; if macro is passed 1 parameter
-            MOV R0,#\1      ; move parameter 1 into R0
+    .macro VDU v1,v2,v3,v4,v5,v6
+        ;.nolist
+        .if \v1<>-1           ; if macro is passed 1 parameter
+            MOV R0,#\v1      ; move parameter 1 into R0
             SWI OS_WriteC   ; write it to the display
-        endif
-        if NARG=2           ; if macro is passed 2 parameters
-            MOV R0,#\1      ; move parameter 1 into R0
+        .endif
+        .if \v2<>-1           ; if macro is passed 2 parameters
+            MOV R0,#\v2      ; move parameter 2 into R0
             SWI OS_WriteC   ; write it to the display
-            MOV R0,#\2      ; move parameter 2 into R0
+        .endif
+        .if \v3<>-1           ; if macro is passed 3 parameters
+            MOV R0,#\v3      ; move parameter 3 into R0
             SWI OS_WriteC   ; write it to the display
-        endif
-        if NARG=3           ; if macro is passed 3 parameters
-            MOV R0,#\1      ; move parameter 1 into R0
+        .endif
+        .if \v4<>-1           ; if macro is passed 4 parameters
+            MOV R0,#\v4      ; move parameter 4 into R0
             SWI OS_WriteC   ; write it to the display
-            MOV R0,#\2      ; move parameter 2 into R0
+        .endif
+        .if \v5<>-1           ; if macro is passed 5 parameters
+            MOV R0,#\v5      ; move parameter 5 into R0
             SWI OS_WriteC   ; write it to the display
-            MOV R0,#\3      ; move parameter 3 into R0
+        .endif
+        .if \v6<>-1           ; if macro is passed 6 parameters
+            MOV R0,#\v6      ; move parameter 6 into R0
             SWI OS_WriteC   ; write it to the display
-        endif
-        if NARG=4           ; if macro is passed 4 parameters
-            MOV R0,#\1      ; move parameter 1 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\2      ; move parameter 2 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\3      ; move parameter 3 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\4      ; move parameter 4 into R0
-            SWI OS_WriteC   ; write it to the display
-        endif
-        if NARG=5           ; if macro is passed 5 parameters
-            MOV R0,#\1      ; move parameter 1 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\2      ; move parameter 2 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\3      ; move parameter 3 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\4      ; move parameter 4 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\5      ; move parameter 5 into R0
-            SWI OS_WriteC   ; write it to the display
-        endif
-        if NARG=6           ; if macro is passed 6 parameters
-            MOV R0,#\1      ; move parameter 1 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\2      ; move parameter 2 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\3      ; move parameter 3 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\4      ; move parameter 4 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\5      ; move parameter 5 into R0
-            SWI OS_WriteC   ; write it to the display
-            MOV R0,#\6      ; move parameter 6 into R0
-            SWI OS_WriteC   ; write it to the display
-        endif
-        list
-        endm
+        .endif
+        ;.list
+    .endm
 
 ; start program for $8000 in memory
-    org $00008000
+    .org 0x00008000
 
 ; jump to start of our code
     B start
 
 ; reserve 256 bytes for a stack
-    dcb.b 256
+    .space 256
 stack:
 
 ; start of our code
 start:
     ADRL SP,stack       ; load stack pointer with our stack address
 
-    VDU VDU_Mode,20                 ; change to mode 20 (640x512 16 colours)
+    VDU VDU_Mode,20,-1,-1,-1,-1     ; change to mode 20 (640x512 16 colours)
     VDU VDU_Palette,0,16,64,64,64   ; set background colour to dark grey
     VDU VDU_Palette,0,24,64,64,64   ; set border colour to dark grey
     VDU VDU_Palette,1,16,255,64,64  ; set colours 1-9 to a rainbow palette
@@ -100,7 +70,7 @@ start:
 
     VDU VDU_Palette,15,16,255,255,255   ; set colour 15 to white
 
-    VDU VDU_TextColour,1    ; select text colour 1
+    VDU VDU_TextColour,1,-1,-1,-1,-1    ; select text colour 1
 
     MOV R0,#65      ; set the character value to display as 'A'
     SWI OS_WriteC   ; write it to the display
@@ -116,42 +86,41 @@ start:
     SWI OS_WriteC   ; write it to the display
     SWI OS_NewLine  ; write a newline to the display
 
-    VDU VDU_TextColour,2    ; select text colour 2
+    VDU VDU_TextColour,2,-1,-1,-1,-1    ; select text colour 2
 
     SWI OS_WriteS       ; write the following string to the display
-    dc.b "Hello World!"
-    dc.b 0
-    align 4             ; align back to the 4 byte boundary
+    .byte "Hello World!",0
+    .align 4             ; align back to the 4 byte boundary
 
-    VDU VDU_TextColour,3    ; select text colour 3
+    VDU VDU_TextColour,3,-1,-1,-1,-1    ; select text colour 3
 
     SWI OS_NewLine  ; write a newline to the display
     SWI OS_WriteS   ; write the following string to the display
-    dc.b 17,1       ; select text colour 1
-    dc.b "Welcome "
-    dc.b 17,2       ; select text colour 2
-    dc.b "to "
-    dc.b 17,3       ; select text colour 3
-    dc.b "ARM "
-    dc.b 17,4       ; select text colour 4
-    dc.b "Assembly "
-    dc.b 17,5       ; select text colour 5
-    dc.b "in "
-    dc.b 17,6       ; select text colour 6
-    dc.b "RISC "
-    dc.b 17,7       ; select text colour 7
-    dc.b "OS "
-    dc.b 17,8       ; select text colour 8
-    dc.b "3. "
-    dc.b 17,9       ; select text colour 9
-    dc.b "BLUE"
-    dc.b 0          ; terminate the string
-    align 4         ; align back to the 4 byte boundary
+    .byte 17,1       ; select text colour 1
+    .byte "Welcome "
+    .byte 17,2       ; select text colour 2
+    .byte "to "
+    .byte 17,3       ; select text colour 3
+    .byte "ARM "
+    .byte 17,4       ; select text colour 4
+    .byte "Assembly "
+    .byte 17,5       ; select text colour 5
+    .byte "in "
+    .byte 17,6       ; select text colour 6
+    .byte "RISC "
+    .byte 17,7       ; select text colour 7
+    .byte "OS "
+    .byte 17,8       ; select text colour 8
+    .byte "3. "
+    .byte 17,9       ; select text colour 9
+    .byte "BLUE"
+    .byte 0          ; terminate the string
+    .align 4         ; align back to the 4 byte boundary
 
     SWI OS_NewLine  ; write a newline to the display
 
 exit:
-    VDU VDU_TextColour,15
-    MOV R0,#0
+    VDU VDU_TextColour,15,-1,-1,-1,-1
+    mov r0,#0xff
     SWI OS_Exit     ; return to RISC OS
 
