5dba735207bc126da70a0bbfb5c7fee956a98bbc
Scott Moore (Arch) <scottmoore@archlinux-epsilon.broadband>
2021-03-01 02:35:33 +0000

    Draw two layer scrolling bitmaps

--- demo.asm
+++ demo.asm
@@ -56,115 +56,80 @@ stack:
 start:
     ADR SP,stack       ; load stack pointer with our stack address
 
-    VDU VDU_Mode,9,-1,-1,-1,-1     ; change to mode 20 (640x512 16 colours)
-    VDU VDU_Palette,0,16,64,64,64   ; set background colour to dark grey
-    VDU VDU_Palette,0,24,64,64,64   ; set border colour to dark grey
-    VDU VDU_Palette,1,16,255,0,0  ; set colours 1-9 to a rainbow palette
-    VDU VDU_Palette,2,16,255,128,0 ; set colours 1-9 to a rainbow palette
-    VDU VDU_Palette,3,16,255,255,0 ; set colours 1-9 to a rainbow palette
-    VDU VDU_Palette,4,16,128,255,0 ; set colours 1-9 to a rainbow palette
-    VDU VDU_Palette,5,16,0,255,0  ; set colours 1-9 to a rainbow palette
-    VDU VDU_Palette,6,16,0,255,128 ; set colours 1-9 to a rainbow palette
-    VDU VDU_Palette,7,16,0,255,255 ; set colours 1-9 to a rainbow palette
-    VDU VDU_Palette,8,16,0,128,255 ; set colours 1-9 to a rainbow palette
-    VDU VDU_Palette,9,16,0,0,255  ; set colours 1-9 to a rainbow palette
+    VDU VDU_Mode,13,-1,-1,-1,-1     ; change to mode 13 (320x256 256 colours)
 
-    VDU VDU_Palette,15,16,255,255,255   ; set colour 15 to white
+    ADR R11,grey
+    ADD R9,R11,#32*32
 
-    VDU VDU_TextColour,1,-1,-1,-1,-1    ; select text colour 1
-
-    MOV R0,#65      ; set the character value to display as 'A'
-    SWI OS_WriteC   ; write it to the display
-    ADD R0,R0,#1    ; increase the character value by 1
-    SWI OS_WriteC   ; write it to the display
-    ADD R0,R0,#1    ; increase the character value by 1
-    SWI OS_WriteC   ; write it to the display
-    ADD R0,R0,#1    ; increase the character value by 1
-    SWI OS_WriteC   ; write it to the display
-    ADD R0,R0,#1    ; increase the character value by 1
-    SWI OS_WriteC   ; write it to the display
-    ADD R0,R0,#1    ; increase the character value by 1
-    SWI OS_WriteC   ; write it to the display
-    SWI OS_NewLine  ; write a newline to the display
-
-    VDU VDU_TextColour,2,-1,-1,-1,-1    ; select text colour 2
-
-    SWI OS_WriteS       ; write the following string to the display
-    .byte "Hello World!",0
-    .align 4             ; align back to the 4 byte boundary
-
-    VDU VDU_TextColour,3,-1,-1,-1,-1    ; select text colour 3
-
-    SWI OS_NewLine  ; write a newline to the display
-    SWI OS_WriteS   ; write the following string to the display
-    .byte 17,1       ; select text colour 1
-    .byte "Welcome "
-    .byte 17,2       ; select text colour 2
-    .byte "to "
-    .byte 17,3       ; select text colour 3
-    .byte "ARM "
-    .byte 17,4       ; select text colour 4
-    .byte "Assembly "
-    .byte 17,5       ; select text colour 5
-    .byte "in "
-    .byte 17,6       ; select text colour 6
-    .byte "RISC "
-    .byte 17,7       ; select text colour 7
-    .byte "OS "
-    .byte 17,8       ; select text colour 8
-    .byte "3. "
-    .byte 17,9       ; select text colour 9
-    .byte "BLUE"
-    .byte 0          ; terminate the string
-    .align 4         ; align back to the 4 byte boundary
-
-    SWI OS_NewLine  ; write a newline to the display
-
-    VDU 19,0,24,0,0,255
+frame_loop:
     ADRL R0,vdu_variables_screen_start
     ADRL R1,buffer
     SWI OS_ReadVduVariables
+    LDR R12,[R1]
+    MOV R10,#256
 
-    MOV R3,#320
-    MOV R4,#40
-    LDR R5,[R1]
-    MLA R12,R3,R4,R5
+    MOV R0,#19
+    SWI OS_Byte
 
-    MOV R0,#0x12340000
-    EOR R0,R0,#0x5600
-    EOR R0,R0,#0x0078
+    VDU 19,0,24,0,0,255
+line_loop:
 
-    MOV R11,#128
-loop:
-    MOV R1,R0
-    MOV R2,R0
-    MOV R3,R0
-    MOV R4,R0
-    MOV R5,R0
-    MOV R6,R0
-    MOV R7,R0
-
+    LDMIA R11!, {R0-R7}
+    
     STMIA R12!, {R0-R7}
     STMIA R12!, {R0-R7}
     STMIA R12!, {R0-R7}
     STMIA R12!, {R0-R7}
     STMIA R12!, {R0-R7}
-    ;STMIA R12!, {R0-R7}
-    ;STMIA R12!, {R0-R7}
-    ;STMIA R12!, {R0-R7}
-    ;STMIA R12!, {R0-R7}
-    ;STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
 
-    MOV R0,R0,ROR #4
+    CMP R11,R9
+    SUBGE R11,R9,#32*32
 
-    SUBS R11,R11,#1
-    BNE loop
+    SUBS R10,R10,#1
+    BNE line_loop
+
+    SUB R11,R11,#32
+
+    VDU 19,0,24,255,0,0
+
+    ADRL R1,buffer
+    LDR R12,[R1]
+    ADD R12,R12,#320*128
+    MOV R10,#32
+
+    MOV R8,R11
+    ADRL R11,grey
+
+layer_loop:
+
+    LDMIA R11!, {R0-R7}
+    
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+    STMIA R12!, {R0-R7}
+
+    SUBS R10,R10,#1
+    BNE layer_loop
+
     VDU 19,0,24,64,64,64
 
+    MOV R11,R8
+
+    B frame_loop
+
 exit:
-    VDU VDU_TextColour,15,-1,-1,-1,-1
-    mov r0,#0x12340000
-    add r0,r0,#0x00005678
     SWI OS_Exit     ; return to RISC OS
 
 vdu_variables_screen_start:
@@ -176,4 +141,6 @@ buffer:
     .4byte 0x00000000
     .4byte 0x00000000
     .4byte 0x00000000
+
+    .include "grey.asm"
 
